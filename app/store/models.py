923d31702cdccb3cf717b7bc69f1a8a993417bfb
IgorBowman <jitser26@gmail.com>
2022-08-26 22:40:34 +0400

    created store, city and street models

--- app/store/models.py
+++ app/store/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    title = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
+
+
+class Street(models.Model):
+    title = models.CharField(max_length=150)
+    city = models.ForeignKey(
+        'City',
+        related_name='city_street',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Store(models.Model):
+    title = models.CharField(max_length=150)
+    city = models.ForeignKey(
+        'City',
+        related_name='cities',
+        on_delete=models.SET_NULL
+    )
+    street = models.ForeignKey(
+        'Street',
+        related_name='streets',
+        on_delete=models.CASCADE
+    )
+    house = models.CharField(max_length=150)
+    time_to_open = models.TimeField(null=True, blank=True)
+    time_to_close = models.TimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
