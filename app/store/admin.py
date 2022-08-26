77ef8ecd2956f67acf3e265e6687c7273d89655b
IgorBowman <jitser26@gmail.com>
2022-08-26 22:42:04 +0400

    registered models in admin.py

--- app/store/admin.py
+++ app/store/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Street, Store, City
 
-# Register your models here.
+admin.site.register(Store)
+admin.site.register(Street)
+admin.site.register(City)
