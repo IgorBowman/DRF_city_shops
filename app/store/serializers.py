a9f699fd45a2a199360f977f70d606ac22047fc1
IgorBowman <jitser26@gmail.com>
2022-09-11 17:48:40 +0400

    added in poststore serializer created method

--- app/store/serializers.py
+++ app/store/serializers.py
@@ -34,3 +34,27 @@ class StoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = Store
         fields = ('title', 'city', 'street', 'house', 'time_to_open', 'time_to_close')
+
+
+class StorePostSerializer(serializers.ModelSerializer):
+    """Serializer for POST request"""
+    city = CitySerializer()
+    street = OnlyStreetNameSerializer()
+
+    class Meta:
+        model = Store
+        fields = ('title', 'city', 'street', 'house', 'time_to_open', 'time_to_close')
+
+    def create(self, validated_data):
+        store_data = validated_data.pop('city', None)
+        store_data2 = validated_data.pop('street', None)
+        if store_data:
+            store_data = Store.objects.get_or_create(**store_data)
+            print(f"STORE DATA: {store_data}")
+            validated_data['city'] = store_data
+        if store_data2:
+            store_data2 = Street.objects.get_or_create(**store_data2)
+            print(f"STORE DATA: {store_data2}")
+            validated_data['street'] = store_data
+
+        return Store.objects.create(**validated_data)
