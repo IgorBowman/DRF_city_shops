b6ef7ce9131effe53c523b70dbbd91c8a2da6650
IgorBowman <jitser26@gmail.com>
2022-09-11 13:23:31 +0400

    refactoring endpoints

--- app/store/urls.py
+++ app/store/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from .views import StoreView, CityView, StreetView, TestCityView
+from .views import CityView, AllStreetsCity, Shop
 
 urlpatterns = [
-    path('cities/', TestCityView.as_view()),
-    path('store/', StoreView.as_view()),
     path('city/', CityView.as_view()),
-    path('city/<int:pk>', CityView.as_view()),
-    path('street/', StreetView.as_view()),
+    path('city/street/', AllStreetsCity.as_view()),
+    path('shop/', Shop.as_view()),
 
 ]
